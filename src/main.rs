a929e224be0f0ad75e3f23f65e4d18129f953de9
Caste00 <davidecastellani000@gmail.com>
2025-05-12 21:22:49 +0200

    Ascii text from an image

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+use crate::img_to_ascii::{img_to_ascii, save_ascii_img, scale_img};
+use std::io::{stdin, Write};
+
+mod img_to_ascii;
+mod pixel_struct;
+
+fn main() {
+    let mut path = String::new();
+    print!("Enter the path to the image: ");
+    std::io::stdout().flush().unwrap();
+    stdin().read_line(&mut path).unwrap();
+    let path = path.trim();
+
+    let mut scale = String::new();
+    print!("Enter the scale of the image: ");
+    std::io::stdout().flush().unwrap();
+    stdin().read_line(&mut scale).unwrap();
+    let scale = scale.trim().parse::<u32>().unwrap();
+
+
+    let pixel = scale_img(path, scale);
+    let pixel_ascii = img_to_ascii(pixel);
+    save_ascii_img(&pixel_ascii);
+}
